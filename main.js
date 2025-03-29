e85ca512d34d7bcaf32f16caee5562babc7333c5
Bartłomiej Pecko <bartlomiejpecko@gmail.com>
2025-03-29 16:12:06 +0100

    Update for release - fixed menu buttons for mobile

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let navbar = document.querySelector('.navbar');
+let menuBtn = document.querySelector('#menu-btn');
 document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 }
@@ -234,3 +235,5 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+
